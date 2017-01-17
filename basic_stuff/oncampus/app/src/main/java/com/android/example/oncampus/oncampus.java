aa9303196411d01f3785b444d97ae3d96cac5e0e
otacke <o.tacke@web.de>
2017-01-17 19:44:00 +0100

    rename file

---
+++ basic_stuff/oncampus/app/src/main/java/com/android/example/oncampus/oncampus.java
@@ -0,0 +1,13 @@
+package com.android.example.oncampus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class oncampus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oncampus);
+    }
+}
